6638481429744b3f64c7a76b7f978f4c0bd7d9ad
Bjorn Lammers <bjorn@lammers.media>
2025-04-12 01:15:45 +0200

    fix: update BASE_URL to include version

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons"
+export const BASE_URL = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons@main"
 export const REPO_PATH = "https://github.com/homarr-labs/dashboard-icons"
