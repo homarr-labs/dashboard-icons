445b7232f2ea3c1cf656ab63b5ba09dad1eea6d2
Thomas Camlong <thomas@ajnart.fr>
2025-04-30 12:29:52 +0200

    update constants

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,9 +1,9 @@
-export const BASE_URL = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons@feat/text-icons"
-export const REPO_PATH = "https://github.com/homarr-labs/dashboard-icons"
+export const BASE_URL = "https://cdn.jsdelivr.net/gh/ajnart/dashboard-icons@wordmark-icons"
+export const REPO_PATH = "https://github.com/ajnart/dashboard-icons"
 // TODO: Change back before merge
-export const METADATA_URL = "https://raw.githubusercontent.com/homarr-labs/dashboard-icons/refs/heads/feat/text-icons/metadata.json"
+export const METADATA_URL = "https://raw.githubusercontent.com/ajnart/dashboard-icons/refs/heads/wordmark-icons/metadata.json"
 export const WEB_URL = "https://dashboardicons.com"
-export const REPO_NAME = "homarr-labs/dashboard-icons"
+export const REPO_NAME = "ajnart/dashboard-icons"
 
 export const getDescription = (totalIcons: number) =>
 	`A collection of ${totalIcons} curated icons for services, applications and tools, designed specifically for dashboards and app directories.`
